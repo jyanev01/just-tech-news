62691c7f8dd386d781658b6387382877fc2a837b
jyanev01 <jordanyanev@gmail.com>
2021-11-24 10:39:32 -0500

    validate use password

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -25,7 +25,14 @@ router.get('/:id', (req, res) => {
         res.status(404).json({ message: 'No user found with this id' });
         return;
       }
-      res.json(dbUserData);
+
+      const validPassword = dbUserData.checkPassword(req.body.password);
+      if(!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!'});
+        return;
+      }
+
+      res.json({ user: dbUserData, message: 'You are now logged in!'});
     })
     .catch(err => {
       console.log(err);
@@ -45,6 +52,24 @@ router.post('/', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.post('/login', (req,res) => {
+  // expects {email: 'lernantiono@gmail.com', password: 'password1234'}
+  User.findOne({ 
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if(!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!'});
+      return;
+    }
+
+    // res.json({ user: dbUserData });
+
+
+  });
 });
 
 router.put('/:id', (req, res) => {
