1042c5f0a58f60dcbd61d27fd5759745addd95df
jyanev01 <jordanyanev@gmail.com>
2021-11-29 06:33:31 -0500

    comments

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
 const postRouter = require('./post-routes.js');
+const commentRoutes = require('./comment-routes.js');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRouter );
+router.use('/comments', commentRoutes);
 
 module.exports = router;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Vote } = require('../../models');
+const { User, Post, Comment, Vote } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
@@ -19,10 +19,18 @@ router.get('/:id', (req, res) => {
     where: {
       id: req.params.id
     },
-    include : [
+    include: [
       {
         model: Post,
         attributes: ['id', 'title', 'post_url', 'created_at']
+      },
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'created_at'],
+        include: {
+          model: Post,
+          attributes: ['title']
+        }
       },
       {
         model: Post,
@@ -59,26 +67,26 @@ router.post('/', (req, res) => {
     });
 });
 
-router.post('/login', (req,res) => {
-  // expects {email: 'lernantiono@gmail.com', password: 'password1234'}
-  User.findOne({ 
+router.post('/login', (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
     where: {
       email: req.body.email
     }
   }).then(dbUserData => {
-    if(!dbUserData) {
-      res.status(400).json({ message: 'No user with that email address!'});
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
       return;
     }
 
     const validPassword = dbUserData.checkPassword(req.body.password);
-    
-    if(!validPassword) {
-      res.status(400).json({ message: 'Incorrect password!'});
+
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
       return;
     }
 
-    res.json({ user: dbUserData, message: 'You are now logged in!'});
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
   });
 });
 
